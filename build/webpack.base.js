2caf2ac69e507821d0e4e4c65eca60b92531a41e
yongyuan <yongyuanliu2022@outlook.com>
2023-06-14 16:37:35 +0800

    babel 语法兼容版本

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -25,7 +25,22 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-react", "@babel/preset-typescript"],
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  // 设置兼容目标浏览器版本,这里可以不写,babel-loader会自动寻找上面配置好的文件.browserslistrc
+                  // "targets": {
+                  //  "chrome": 35,
+                  //  "ie": 9
+                  // },
+                  useBuiltIns: "usage", // 根据配置的浏览器兼容,以及代码中使用到的 api 进行引入 polyfill 按需添加
+                  corejs: 3, // 配置使用 core-js 版本
+                },
+              ],
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
           },
         },
       },
